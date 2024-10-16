bb1e8ea7d186b5b16373e267d78444b4ad4c0660
victor <victorsoaresferreira09@gmail.com>
2024-10-16 15:24:06 -0300

    mudando a classe filme

--- src/Principal.java
+++ src/Principal.java
@@ -1,14 +1,17 @@
+import br.com.victor.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome =  "Exterminador do Futuro";
-        meuFilme.anoDeLancamento = 1700;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("Exterminador do futuro");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
 
         meuFilme.exibiFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(9);
         meuFilme.avalia(10);
-        System.out.println((meuFilme.mediaDasAvaliacao()));
+        System.out.println(meuFilme.mediaDasAvaliacao());
+        // meuFilme.somaDasAvaliacoes = 10;
     }
 }
